fd6eb342383f6f99c0b667c44370a2e418e9f958
Nihal Reddy <reddyn2k6@gmail.com>
2025-12-26 19:51:07 +0530

    Some changes

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-import express from "express"
-import connectDB from './config/connectDB.js'
-import 'dotenv/config.js';
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/connectDB.js";
+import "dotenv/config.js";
 import router from "./routes/people.js";
-const app=express();
 
-const PORT=4000;
+const app = express();
+const PORT = 4000;
+
+// ✅ Allow everything from everywhere
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 connectDB();
 
-app.use('/person',router);
+app.use("/person", router);
 
-app.listen(PORT,()=>{
-    console.log("Server Started on ",PORT);
-})
+app.listen(PORT, () => {
+  console.log("Server Started on", PORT);
+});
